d9b20d342e9195224e33f1df438ce441573bde40
VictorGutierrez_ <vmgf09@gmail.com>
2022-11-10 22:12:59 -0600

    Inicializacion
    Inicializacion de proyecto base.
    ** Vistas agregadas

---
+++ resources/views/crearequipos.blade.php
@@ -0,0 +1,12 @@
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RULE</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <p>Hola mundo</p>
+</body>
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>RULE</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body class="">
+    <!--<div class="bg-fixed" style="background-image:url(img/bg-numeros);background-size: 70%; background-position:left top;">-->
+        <div class="space-y-5 pb-8 bg-fixed" style="background-image:url(/img/bg-numeros.jpg);background-size: 35%; background-position:left top;background-repeat: no-repeat;">
+            <div class="max-w-sm mx-auto">
+                <img src="img/logo_rule.png" alt="Logo rule" class="py-10">
+            </div>
+            <div class="p-4 max-w-sm mx-auto bg-[#D61C4E] rounded-3xl flex items-center space-x-4 text-white text-right">
+                <div class="shrink-0">
+                    <img class="h-20 w-20" src="img/mas.png" alt="Signo mas">
+                </div>
+                <div class="">
+                    <div class="text-2xl font-medium">Agregar Equipos</div>
+                    <p class="font-normal">Maximo 3 equipos</p>
+                </div>
+            </div>
+        </div>
+    <!--</div>-->
+    <footer class="bg-orange-500 py-6 mt-8 text-right">
+        <div class="font-semibold pr-4">Benemerita Universidad Autonoma de Puebla</div>
+        <div class="font-light pr-4">Facultad de Ciencias de la Computación</div>
+    </footer>
+</body>
+
+</html>
