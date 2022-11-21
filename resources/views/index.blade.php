d6476a694963e20386c119a9b510bffbccef19de
EduCastCh <eduardocastch@gmail.com>
2022-11-20 18:45:28 -0600

    cambios sobre diseño
    pagina principal y de seleccion de equipos estilizado

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,46 +1,20 @@
-<!DOCTYPE html>
-<html lang="es">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>RULE</title>
-    @vite('resources/css/app.css')
-</head>
-
-<body class="">
-    <!--<div class="bg-fixed" style="background-image:url(img/bg-numeros);background-size: 70%; background-position:left top;">-->
-        <div class="space-y-5 pb-8 bg-fixed" style="background-image:url(/img/bg-numeros.jpg);background-size: 35%; background-position:left top;background-repeat: no-repeat;">
+@extends('layout')
+    @section('content')
+        <div class="space-y-5 pb-8 bg-fixed flex-col" style="background-image:url(/img/bg-numeros.jpg);background-size: 40%; background-position:left top;background-repeat: no-repeat;">
+            <!---Logo-->
             <div class="max-w-sm mx-auto">
                 <img src="img/logo_rule.png" alt="Logo rule" class="py-10">
             </div>
-<<<<<<< Updated upstream
-            <div class="p-4 max-w-sm mx-auto bg-[#D61C4E] rounded-3xl flex items-center space-x-4 text-white text-right">
-=======
             <!--BUTON DE INCIO-->
-            <div class="p-1 max-w-sm w-72 mx-auto bg-[#D61C4E] rounded-3xl flex items-center space-x-4 text-white text-right transition ease-in-out delay-150  hover:-translate-1 hover:scale-105 hover:bg-[#D61C4E] duration-150">
->>>>>>> Stashed changes
+            <div class="p-1 max-w-sm w-52 mx-auto bg-[#D61C4E] rounded-3xl flex items-center space-x-4 text-white text-right transition ease-in-out delay-150  hover:-translate-1 hover:scale-105 hover:bg-[#D61C4E] duration-150">
                 <div class="shrink-0">
                     <img class="h-16 w-16" src="img/mas.png" alt="Signo mas">
                 </div>
-<<<<<<< Updated upstream
-                <div class="">
-                    <div class="text-2xl font-medium">Agregar Equipos</div>
-                    <p class="font-normal">Máximo 3 equipos</p>
-=======
                 <div class="text-2xl font-medium w-auto">
                     <a href="/crearequipos">
-                        Agregar Equipos
+                        Iniciar
                     </a>
->>>>>>> Stashed changes
                 </div>
             </div>
         </div>
-    <!--</div>-->
-    <footer class="bg-orange-500 py-6 mt-8 text-right">
-        <div class="font-semibold pr-4">Benemérita Universidad Autónoma de Puebla</div>
-        <div class="font-light pr-4">Facultad de Ciencias de la Computación</div>
-    </footer>
-</body>
-
-</html>
+    @endsection
