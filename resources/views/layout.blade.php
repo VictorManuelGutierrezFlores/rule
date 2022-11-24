b69a8c717323ad905f6101522968520562013369
Atenas <atenasmoore@gmail.com>
2022-11-24 16:49:43 -0600

    Diseño
    modificaciones de diseños y creación de tercera vista de juego comenzado

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -11,9 +11,15 @@
 <body class="">
     @yield('content')
     <!--PIE DE PAGINA-->
+<<<<<<< Updated upstream
     <footer class="bg-orange-400 py-6 mt-8 text-right text-xl">
         <div class="font-semibold pr-4">Benemerita Universidad Autonoma de Puebla</div>
         <div class="font-light pr-4">Facultad de Ciencias de la Computación</div>
+=======
+    <footer class="bg-[#293462] text-white py-6 mt-8 text-right text-xl w-full absolute bottom-0">
+        <div class="font-just text-4xl pr-4">Benémerita Universidad Autónoma de Puebla</div>
+        <div class="font-just text-3xl pr-4">Facultad de Ciencias de la Computación</div>
+>>>>>>> Stashed changes
     </footer>
 </body>
 </html>
