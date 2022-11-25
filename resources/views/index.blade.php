4a2a7ec97ec29ec82d602a691a09a76daae140bb
EduCastCh <eduardocastch@gmail.com>
2022-11-25 00:53:05 -0600

    juego y ganadores
    se agregaron las vistas del juego en progreso y las vistas correspondientes a los ganadores y sus variaciones. Aun falta poner funcionalidad

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,18 +3,15 @@
         <div class="space-y-5 pb-8 bg-fixed flex-col" style="background-image:url(/img/bg-numeros.jpg);background-size: 40%; background-position:left top;background-repeat: no-repeat;">
             <!---Logo-->
             <div class="max-w-sm mx-auto">
-                <img src="img/logo_rule.png" alt="Logo rule" class="py-10">
+                <img src="img/logo_rule-png.png" alt="Logo rule" class="py-10">
             </div>
             <!--BUTON DE INCIO-->
-            <div class="p-1 max-w-sm w-52 mx-auto bg-[#D61C4E] rounded-3xl flex items-center space-x-4 text-white text-right transition ease-in-out delay-150  hover:-translate-1 hover:scale-105 hover:bg-[#D61C4E] duration-150">
-                <div class="shrink-0">
-                    <img class="h-16 w-16" src="img/mas.png" alt="Signo mas">
-                </div>
-                <div class="text-2xl font-medium w-auto">
-                    <a href="/crearequipos">
+            <a href="/crearequipos">
+            <div class="button p-1 max-w-sm w-52 mx-auto bg-[#D61C4E] rounded-3xl flex items-center space-x-4 text-white transition ease-in-out delay-150  hover:-translate-1 hover:scale-105 hover:bg-[#D61C4E] duration-150">
+                <div class="font-medium w-auto m-auto font-just text-6xl">
                         Iniciar
-                    </a>
                 </div>
             </div>
+            </a>
         </div>
     @endsection
